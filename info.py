e48bcd24f9cd3e4ceb1eadffd693742a8f9a57c8
Skssssee <91522489+Skssssee@users.noreply.github.com>
2025-07-03 14:27:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -40,7 +40,7 @@ CHANNELS = [
 # ForceSub Channel & Log Channels
 AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1002821475759"))
 AUTH_REQ_CHANNEL = int(environ.get("AUTH_REQ_CHANNEL", "-1002837276813"))
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002684140912"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002295313217"))
 LOG_API_CHANNEL = int(environ.get("LOG_API_CHANNEL", "-1002584596532"))
 LOG_VR_CHANNEL = int(environ.get("LOG_VR_CHANNEL", "-1002759029438"))
 
