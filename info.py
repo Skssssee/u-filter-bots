bdf4f26b59465eeb2b85527cca3bab8140098760
Skssssee <91522489+Skssssee@users.noreply.github.com>
2025-07-06 17:00:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -64,8 +64,8 @@ REQUEST_CHANNEL = (
 MOVIE_UPDATE_CHANNEL = int(environ.get("MOVIE_UPDATE_CHANNEL", "-1001864434358"))
 
 # Added Link Here Not Id
-SUPPORT_CHAT = environ.get("SUPPORT_CHAT", "https://t.me/cinexhub")
-MOVIE_GROUP_LINK = environ.get("MOVIE_GROUP_LINK", "https://t.me/cinexhub_official")
+SUPPORT_CHAT = environ.get("SUPPORT_CHAT", "https://t.me/cinexhub_official")
+MOVIE_GROUP_LINK = environ.get("MOVIE_GROUP_LINK", "https://t.me/cinexhub")
 
 # Verification
 IS_VERIFY = is_enabled("IS_VERIFY",False)
