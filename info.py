55d80187f141c009853ec93ded5258a497455a33
Skssssee <91522489+Skssssee@users.noreply.github.com>
2025-11-03 12:09:07 +0530

    Update default URL for the application

--- info.py
+++ info.py
@@ -173,7 +173,7 @@ if "DYNO" in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "https://desperate-shandra-ty667-ae16403f.koyeb.app/")
+URL = environ.get("FQDN", "https://obliged-tanitansy-ty667-bc36f783.koyeb.app/")
 
 # Commands
 admin_cmds = [
